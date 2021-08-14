daafc881c12f502ee5ba8f0072891deb1fea9e69
Tom O'Connell <toconnel@caltech.edu>
2021-08-13 20:55:48 -0700

    Update installation instructions and add script to automate most of setup

--- README.md
+++ README.md
@@ -1,21 +1,50 @@
 
 #### Installation
 
-Only tested on Ubuntu 18.04. Python 3.7 and earlier seemed to have some
-incompatibilities with `suite2p`.
+##### Manual
 
 ```
-sudo apt update
-# TODO also install necessary apt packages for qt5 backend of pyqt5
-sudo apt install python3.8-minimal python3.8-venv
+mkdir -p ~/src
+cd ~/src
 
-python3.8 -m venv venv
-source venv/bin/activate
-pip install --upgrade pip
+# My modified version of suite2p
+git clone https://github.com/tom-f-oconnell/suite2p
 
-git clone https://github.com/ejhonglab/hong2p ../hong2p
-pip install -e ../hong2p
+git clone https://github.com/ejhonglab/hong2p
+git clone https://github.com/ejhonglab/al_pair_grid
 
-pip install suite2p
+# This will make a conda environment named 'suite2p'
+conda env create -f suite2p/environment.yml
+
+conda activate suite2p
+cd ~/src/al_pair_grid
+pip install -r requirements.txt
+```
+
+Then set the environment variables `HONG_NAS` and `HONG2P_FAST_DATA_DIR` appropriately.
+
+
+##### Mostly automated
+
+To install as manual install above, but using SSH authentication, you may simply call
+`./install.sh` after cloning. If it asks you to set environment variables, you will need
+to manually do so.
+
+Only tested on Ubuntu 18.04.
+
+
+##### Notes
+
+Note that despite the fact that `suite2p/environment.yml` includes the line `suite2p`,
+my current conda installation (despite `conda clean -a` and deleting all but the base
+environment) thinks it already has `suite2p` when it gets to that part of the install
+step. This is only really an issue because it suggests some other dependencies may not
+be installed correctly, as I would otherwise want to prevent suite2p from being
+installed, so that I can install an editable version of my fork without any ambiguity
+about other versions possibly being installed.
+
+This is the relevant line:
+```
+Requirement already satisfied: suite2p in /home/tom/src/suite2p (from -r /home/tom/src/suite2p/      condaenv.npju4l22.requirements.txt (line 15)) (0.10.2.dev11+gc7af998.d20210802)
 ```
 
---
+++ install.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# Assumes the following are already installed:
+# - conda
+# - git
+# - awk
+
+# Defaults to suite2p, but if you call as (for example) `./install.sh al_pair_grids`,
+# this will take the value 'al_pair_grids'
+conda_env_to_make=${1:-suite2p}
+
+src_path=$HOME/src
+mkdir -p ${src_path}
+cd ${src_path}
+
+declare -a required_git_repos=(
+    # My modified version of suite2p
+    "tom-f-oconnell/suite2p"
+
+    "ejhonglab/hong2p"
+
+    # Chances are we are running this script because we have already cloned this, but
+    # leaving it in to enable one-line install getting the script via curl or something
+    # (would require making this repo public).
+    "ejhonglab/al_pair_grids"
+)
+
+git_auth_prefix="git@github.com:"
+
+# Assuming everything is suitably up-to-date if already cloned.
+for repo in "${required_git_repos[@]}"
+do
+    # https://stackoverflow.com/questions/17921544
+    dir_name=$(echo $repo | awk -F/ '{print $NF}')
+    if [ ! -d $dir_name ]; then
+        git clone ${git_auth_prefix}${repo}
+    fi
+done
+
+if conda env list | awk '{print $1}' | grep -q "^${conda_env_to_make}$"; then
+    echo "Environment ${conda_env_to_make} already exists! Pass alternate name, e.g. \`./install.sh al_pair_grids\`"
+    exit 1
+fi
+
+# This would make a conda environment named 'suite2p' if we didn't pass -n explicitly
+conda env create -f suite2p/environment.yml -n ${conda_env_to_make}
+
+conda activate ${conda_env_to_make}
+cd ~/src/al_pair_grids
+# TODO test that this actually works (that conda activate call before actually worked in
+# the script context. if not, need to do this a different way)
+pip install -r requirements.txt
+
+declare -a required_env_vars=(
+    # There are some other environment variables that can directly specify raw data root
+    # without any mention of NAS, but I haven't been using those anyway.
+    "HONG_NAS"
+
+    "HONG2P_FAST_DATA_DIR"
+)
+declare -a example_values=(
+    # Should contain 'mb_team' directory immediately under it.
+    "/path/to/nas"
+
+    # Should contain 'raw_data' immediately under it.
+    # Data will preferentially be loaded from here, and analysis intermediates will be
+    # written to an 'analysis_intermediates' subdirectory that will be created here.
+    "/local/copy/of/raw/data"
+)
+for i in "${!required_env_vars[@]}"
+do
+    env_var=${required_env_vars[i]}
+    if [ -z "${!env_var}" ]; then
+        echo "Set environment variable ${env_var} by adding a line to your ~/.bashrc such as:"
+        echo "export ${env_var}=\"${example_values[i]}\""
+        echo ""
+    fi
+done
+
