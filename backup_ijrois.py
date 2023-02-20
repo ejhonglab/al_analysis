bb0d20afff38f12aefcc8388b2f520f862e8ccdd
Tom O'Connell <toconnel@caltech.edu>
2023-02-20 14:46:18 -0800

    backup_ijrois.py: also saving backups to central location set by env var, backup filenames now using backup time as well as RoiSet.zip creation time.

--- backup_ijrois.py
+++ backup_ijrois.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 """
 Copies ImageJ ROIs in each recording directory to a backup subdirectory,
-with timestamp ROIs were created in name of backup file.
+with timestamp of backup and when ROIs were created in name of backup files.
+
+Will also backup to a central directory if the AL_IJROI_BACKUP_DIR environment variable
+is set.
 """
 
 from argparse import ArgumentParser
 from datetime import datetime
+from os import getenv
 from pathlib import Path
 from pprint import pprint
 from shutil import copy2
@@ -14,6 +18,10 @@ from zipfile import is_zipfile, ZipFile
 from ijroi import zip_contents_equal
 
 from hong2p.util import analysis_intermediates_root
+
+
+def format_time(timestamp: datetime) -> str:
+    return timestamp.strftime('%Y-%m-%d_%H%M%S')
 
 
 def main():
@@ -27,10 +35,23 @@ def main():
     verbose = args.verbose > 0
     extra_verbose = args.verbose > 1
 
-    # TODO option to also backup to a central location?
-
+    # The filename we are looking for under analysis_root, to be backed up.
     roi_zip_name = 'RoiSet.zip'
+
+    # The sub-directory (within each experiments analysis directory) to be created and
+    # used for backups of the RoiSet.zip files.
     backup_dir_name = 'ijroi_backups'
+
+    central_backup_dir_envvar = 'AL_IJROI_BACKUP_DIR'
+    central_backup_dir_root = getenv(central_backup_dir_envvar)
+
+    # If True, will delete any backups with equivalent content (minus the timestamps...)
+    # before saving them once again. For changing naming convention / etc.
+    # Does not apply to central backups.
+    _replace_equiv_backups = False
+
+    if _replace_equiv_backups:
+        verbose = True
 
     # if i ever actually use both a fast and non-fast analysis intermediate directory,
     # would need to also check the non-fast one
@@ -50,6 +71,27 @@ def main():
     print(f'checking for ImageJ ROIs ({roi_zip_name}) under {analysis_root}')
     if verbose:
         print()
+
+    backup_time = datetime.now()
+
+    if central_backup_dir_root is not None:
+        central_backup_dir_root = Path(central_backup_dir_root)
+        assert central_backup_dir_root.is_dir()
+
+        central_backup_dir_root = central_backup_dir_root / backup_dir_name
+        central_backup_dir_root.mkdir(exist_ok=True)
+
+        if verbose:
+            print(f'also backing up ROIs to central {central_backup_dir_root=}\n')
+
+        central_backup_dir = central_backup_dir_root / format_time(backup_time)
+        central_backup_dir.mkdir(exist_ok=False)
+    else:
+        if verbose:
+            print('not additionally backing up ROIs to a central directory '
+                f'(env var {central_backup_dir_envvar} not set)\n'
+            )
+        central_backup_dir = None
 
     roi_zip_glob_str = f'*/*/*/{roi_zip_name}'
 
@@ -75,8 +117,8 @@ def main():
 
             continue
 
-        # in case there is a symlink in the path or something, we still know a given path
-        # always refers to the same actual file, when considering the set we've seen
+        # in case there is a symlink in the path, we still know a given path always
+        # refers to the same actual file, when considering the set we've seen
         roi_zip = roi_zip.resolve()
         seen_roi_zips.add(roi_zip)
 
@@ -84,21 +126,32 @@ def main():
         backup_dir.mkdir(exist_ok=True)
 
         make_backup = True
-        for existing_backup_zip in backup_dir.glob('*.zip'):
+        for existing_backup in backup_dir.glob('*.zip'):
             # This checks if ZIP files are equal, but intentially does NOT check
             # timestamps, so that if ROIs are loaded and re-saved (without changing)
             # a new backup does not need to be created.
-            if zip_contents_equal(roi_zip, existing_backup_zip):
+            if zip_contents_equal(roi_zip, existing_backup):
                 if verbose:
-                    print(f'already backed up at {format_path(existing_backup_zip)}')
+                    print(f'already backed up at {format_path(existing_backup)}')
+
+                if _replace_equiv_backups:
+                    if verbose:
+                        print('deleting existing equivalent backup')
+
+                    existing_backup.unlink()
+                    continue
 
                 make_backup = False
                 break
 
         if not make_backup:
-            if verbose:
-                print()
-            continue
+            # Since we want all ROIs to be included in each of the timestamp-named
+            # directories under central backup root.
+            if central_backup_dir is None:
+
+                if verbose:
+                    print()
+                continue
 
         with ZipFile(roi_zip, 'r') as zip_file:
             if extra_verbose:
@@ -116,22 +169,44 @@ def main():
         # Using this, rather than mtime of .zip file, as copying ROIs (without
         # preserving mtime) could give the wrong impression about when the ZIP was saved
         # from ImageJ.
-        timestamp = datetime(*zip_times.pop())
+        save_time = datetime(*zip_times.pop())
+        save_time_fname = f'{format_time(save_time)}-creation_{roi_zip_name}'
 
-        backup_roi_zip = backup_dir / (f'{timestamp:%Y-%m-%d_%H%M%S}_{roi_zip_name}')
-        if verbose:
-            print(f'copying to {format_path(backup_roi_zip)}')
+        if make_backup:
+            backup = (
+                backup_dir / f'{format_time(backup_time)}-backup_{save_time_fname}'
+            )
+            if verbose:
+                print(f'copying to {format_path(backup)}')
 
-        # copy2 preserves modification time (among other things).
-        copy2(roi_zip, backup_roi_zip)
-        backed_up_this_run.add(roi_zip)
+            # copy2 preserves modification time (among other things).
+            copy2(roi_zip, backup)
+            backed_up_this_run.add(roi_zip)
+
+        if central_backup_dir is not None:
+            # E.g. '2021-04-28_1_butanal_and_acetone',
+            # from 2021-04-28/1/butanal_and_acetone
+            experiment_id_prefix = '_'.join(
+                roi_zip.relative_to(analysis_root).parent.parts
+            )
+            # We already have the backup time in the name of central_backup_dir, so no
+            # need to also include in filenames here.
+            central_backup = (
+                central_backup_dir / f'{experiment_id_prefix}_{save_time_fname}'
+            )
+            if verbose:
+                # NOTE: can't use format_path here, as central backup dir not under
+                # analysis root
+                print(f'copying to central backup at {central_backup}')
+
+            copy2(roi_zip, central_backup)
 
         if verbose:
             print()
 
 
     all_zip_files = set()
-    # TODO This also match stuff WITHOUT stuff after the '.zip' part?
+    # TODO this also match stuff WITHOUT stuff after the '.zip' part?
     for zip_path in analysis_root.rglob(f'*.zip*'):
         if zip_path.is_symlink():
             continue
@@ -179,8 +254,7 @@ def main():
     if len(backed_up_this_run) == 0:
         print('nothing new to backup')
     else:
-        print('backed up:')
-        pprint({format_path(x) for x in backed_up_this_run})
+        print('newly backed up:')
         print_paths(backed_up_this_run)
 
 
